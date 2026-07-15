98277e3091b7542cbf69e7ffd69ca13702c0e5a3
Thomas Dettmar <thomasdettmar@outlook.com>
2026-07-14 22:00:04 -0700

    Chamber Directory Fix

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -4,11 +4,9 @@ const navigation = document.querySelector("#primaryNav");
 menuBtn.addEventListener("click", () => {
     navigation.classList.toggle("open");
 
-    if (navigation.classList.contains("open")) {
-        menuBtn.textContent = "✕";
-        menuBtn.setAttribute("aria-label", "Close Navigation");
-    } else {
-        menuBtn.textContent = "☰";
-        menuBtn.setAttribute("aria-label", "Open Navigation");
-    }
+    const expanded = navigation.classList.contains("open");
+
+    menuBtn.setAttribute("aria-expanded", expanded);
+
+    menuBtn.textContent = expanded ? "✕" : "☰";
 });
